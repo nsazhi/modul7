6fe0eea31ebd4a8b3391d0ab7a1003660a928dee
nsazhi <n190275@inbox.ru>
2024-08-23 16:47:50 +0400

    module_7_1.py

--- module_7_1.py
+++ module_7_1.py
@@ -1,6 +1,3 @@
-from pprint import pprint
-
-
 class Product:
     def __init__(self, name, weight, category):
         self.name = name
@@ -36,7 +33,7 @@ p1 = Product('Potato', 50.5, 'Vegetables')
 p2 = Product('Spaghetti', 3.4, 'Groceries')
 p3 = Product('Potato', 5.5, 'Vegetables')
 
-print(p2) # __str__
+print(p2)  # __str__
 
 s1.add(p1, p2, p3)
 
