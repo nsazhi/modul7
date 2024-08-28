abbed93a914a919f4f38053962b03ea72b1c4f4e
nsazhi <n190275@inbox.ru>
2024-08-28 16:30:11 +0400

    Add files via upload

---
+++ module_7_3.py
@@ -0,0 +1,47 @@
+import string
+from operator import index
+
+
+class WordsFinder:
+    file_names = []
+
+    def __init__(self, *args):
+        self.file_names.extend(args)
+
+    def get_all_words(self):
+        all_words = {}
+        for el in self.file_names:
+            with open(el, encoding='utf-8') as file:
+                file_line = ' '.join(file.read().splitlines())
+                file_line = ''.join(ch for ch in file_line if ch not in set(string.punctuation))
+                file_line = file_line.lower().split()
+            all_words[el] = file_line
+        return all_words
+
+    def upd_word(self, word):
+        word = ''.join(ch for ch in word if ch not in set(string.punctuation))
+        word = word.lower()
+        return word
+
+    def find(self, word):
+        word = self.upd_word(word)
+        for key, value in self.get_all_words().items():
+            if word in value:
+                i = value.index(word) + 1
+                return {key: i}
+
+    def count(self, word):
+        word = self.upd_word(word)
+        for key, value in self.get_all_words().items():
+            if word in value:
+                count_ = value.count(word)
+                return {key: count_}
+
+
+finder1 = WordsFinder('test_file1.txt')
+finder2 = WordsFinder('test_file2.txt')
+print(finder2.get_all_words())
+print(finder1.find('cocos'))
+print(finder2.find('TEXT'))
+print(finder2.find('It`s'))
+print(finder2.count('teXT'))
